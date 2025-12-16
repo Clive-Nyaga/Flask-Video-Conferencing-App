f22ddacdfd92a0d9dc142337d8e3bca290fe0101
Clive Nyaga <nyagakagotho@gmail.com>
2025-12-17 00:44:57 +0300

    Creates database for user details

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Length
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///video-meeting.db'
+db.init_app(app)
+
+class Register(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    # def __repr__(self):
+    #     return f'<User {self.username}>'
+
+    def is_active(self):
+        return True
+    
+    def get_id(self):
+        return str(self.id)
+
+
+with app.app_context():
+    db.create_all()
 
 class RegistrationForm(FlaskForm):    
     email = EmailField('Email', validators=[DataRequired()])
@@ -21,17 +47,64 @@ class LoginForm(FlaskForm):
 
 @app.route('/')
 def home():
-    return ("Hello, World!")
+    return redirect(url_for('login'))
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     form = LoginForm()
+    if request.method == "POST" and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = Register.query.filter_by(email=email, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        # else:
+        #    flash("Invalid email or password. Please try again.")
+
     return render_template("login.html", form=form)
 
-@app.route("/register")
+@app.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    flash("You have been logged out successfully!")
+    return redirect(url_for('login'))
+
+@app.route("/register", methods = ["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if request.method == "POST" and form.validate_on_submit():
+        new_user = Register(
+            email = form.email.data,
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            username = form.username.data,
+            password = form.password.data
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account created successfully! <br>You can now log in.")
+        return redirect(url_for('login'))
+
     return render_template("register.html", form=form)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", first_name=current_user.first_name, last_name=current_user.last_name)
+
+@app.route("/meeting")
+def meeting():
+    return render_template("meeting.html")
+
+@app.route("/join")
+def join():
+    return render_template("join.html")
+
+# @app.route("/logout")
+# def logout():
+#     logout_user()
+#     return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
