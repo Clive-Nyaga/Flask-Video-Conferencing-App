c60eb6cefa715a54157205bd616c860859410812
Clive Nyaga <nyagakagotho@gmail.com>
2025-12-16 16:52:30 +0300

    Sets up input fields for the registration and login forms

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_secret_key'
+
+class RegistrationForm(FlaskForm):    
+    email = EmailField('Email', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    # submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    # submit = SubmitField('Login')
 
 @app.route('/')
 def home():
@@ -7,11 +25,13 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    return render_template("register.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
